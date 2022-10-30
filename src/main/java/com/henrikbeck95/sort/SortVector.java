80901f542962909e8f9bc34f0dca996232726c76
Henrik Beck <henrikbeck95@gmail.com>
2022-10-30 14:11:20 -0300

    SortVector class has been implemented with JUnit5 tests

---
+++ src/main/java/com/henrikbeck95/sort/SortVector.java
@@ -0,0 +1,19 @@
+package com.henrikbeck95.sort;
+
+import com.henrikbeck95.sort.algorithms.QuickSort;
+
+public class SortVector {
+	public static int[] sortNormal(int array[]) {
+		return QuickSort.main(array);
+	}
+
+	public static int[] sortReverse(int array[]) {
+		int aux[] = new int[array.length];
+
+		for (int i = 0; i < array.length; i++) {
+			aux[i] = array[(array.length - i) - 1];
+		}
+
+		return aux;
+	}
+}
---
+++ src/test/java/com/henrikbeck95/sort/SortVectorTest.java
@@ -0,0 +1,23 @@
+package com.henrikbeck95.sort;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SortVectorTest {
+	@Test
+	void sortNormal() {
+		int[] actual = { 9, 5, 1, 0, 6, 2, 3, 4, 7, 8 };
+		int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		assertArrayEquals(SortVector.sortNormal(actual), expected);
+	}
+
+	@Test
+	void sortReverse() {
+		int[] actual = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int[] expected = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+		assertArrayEquals(SortVector.sortReverse(actual), expected);
+	}
+}
